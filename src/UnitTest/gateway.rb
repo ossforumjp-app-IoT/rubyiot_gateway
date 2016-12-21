000386a003ca31debad910bbb013c8963c51f2ec
TomofumiTasaka <tasaka.tomofumi@jp.fujitsu.com>
2016-12-21 21:01:22 +0900

    ログ機能の追加

--- src/UnitTest/gateway.rb
+++ src/UnitTest/gateway.rb
@@ -10,27 +10,27 @@ class TestDataHandler < Minitest::Test #Test::Unit::TestCase
 
   @gateway_id = 1
 
-  def test_typecheck
-    puts "====================================="
-    puts __method__
+#  def test_typecheck
+#    puts "====================================="
+#    puts __method__
 #    assert_nothing_raised( RuntimeError ) { Gateway.new(@gateway_id) }
-    gateway_id = 1
-    Gateway.new(gateway_id)
-  end
+#    gateway_id = 1
+#    Gateway.new(gateway_id)
+#  end
 
-  def test_def_threads_pool
-    puts "====================================="
-    puts __method__
-    gateway_id = 1
-    puts Gateway.new(gateway_id).def_threads_mapping()
-  end
+#  def test_def_threads_pool
+#    puts "====================================="
+#    puts __method__
+#    gateway_id = 1
+#    puts Gateway.new(gateway_id).def_threads_mapping()
+#  end
 
 
   def test_main
     puts "====================================="
     puts __method__
     gateway_id = 1
-    puts Gateway.new(gateway_id).main()
+    Gateway.new(gateway_id).main()
   end
 
 end
